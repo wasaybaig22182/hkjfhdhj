d178699f3bdbc090aab612d3a658d1550cbb9388
wasaybaig22182 <mirzawasaybaig6@gmail.com>
2024-08-23 18:52:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+for(var i=0;0<=10;i++)
+{
+    console.log("wasau");
+}
+var table = 12;
+var range = 10;
+for(var i=1;i<=range;i++)
+{
+    alert(table + "*" + i + "=" + table*i);
+}
+
+while(i<=10)
+{
+    text += "2" +i;i++;
+}
